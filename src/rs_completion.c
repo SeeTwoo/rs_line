650e8225e0aeaea868d23a698b87f2438060a788
SeeTwoo <walti.bee@gmail.com>
2025-10-29 17:57:45 +0100

    we have basic completion LEZZGO

--- src/rs_completion.c
+++ src/rs_completion.c
@@ -14,90 +14,82 @@
 #include <stdio.h>
 #include <string.h>
 #include <dirent.h>
+#include <unistd.h>
 
 #include "ts_readline_struct.h"
 
-int	is_sep(char c) {
-	return (
-		c == '>' ||
-		c == '<' ||
-		c == '|' ||
-		c == '&' || 
-		c == ' '
-	);
+#ifndef DELIM
+# define DELIM "<>&| "
+#endif
+
+typedef struct s_comp	t_comp;
+
+struct s_comp {
+	char	*word_start;
+	char	*word_end;
+	char	*last_slash;
+	char	*base;
+	size_t	base_len;
+	char	*filler;
+};
+
+int	is_delim(char c) {
+	return ( c == '>' || c == '<' || c == '|' || c == '&' || c == ' ');
 }
 
-char	*find_current_word(char *line, int i) {
-	while (i > 0 && !is_sep(line[i]))
-		i--;
-	return (line + i);
-}
-
-char	*find_last_slash(char *word) {
-	char	*last_slash = NULL;
-
-	while (*word && !is_sep(*word)) {
-		if (*word == '/')
-			last_slash = word;
-		word++;
-	}
-	return (last_slash);
-}
-
-char	*dir_substring(char *dir, char *last_slash) {
-	if (last_slash)
-		return (strndup(dir, last_slash - dir + 1));
-	else
-		return (strdup("."));
-}
-
-char	*file_substring(char *path, char *last_slash) {
-	if (last_slash)
-		return (strndup(last_slash + 1, strcspn(last_slash + 1, " <>&|")));
-	else
-		return (strndup(path, strcspn(path, " <>&|")));
-}
-
-char	*find_match(char *to_complete, char *last_slash/*, char *replaced*/) {
-	char	*dir_name = dir_substring(to_complete, last_slash);
-	char	*foot = file_substring(to_complete, last_slash);
-	DIR		*dir;
+char	*find_match(t_comp *comp) {
+	char			*dir_name;
+	DIR				*dir;
 	struct dirent	*entry;
 
-	if (!dir_name || !foot)
-		return (free(dir_name), free(foot), NULL);
+	if (comp->last_slash)
+		dir_name = strndup(comp->word_start, comp->last_slash + 1 - comp->word_start);
+	else
+		dir_name = strdup(".");
+	if (!dir_name)
+		return (NULL);
 	dir = opendir(dir_name);
 	free(dir_name);
 	if (!dir)
 		return (NULL);
 	entry = readdir(dir);
 	while (entry) {
-		if (strncmp(foot, entry->d_name, strlen(foot)) == 0)
-			return (free(foot), entry->d_name);
+		if (strncmp(comp->base, entry->d_name, comp->base_len) == 0)
+			return (entry->d_name + comp->base_len);
 		entry = readdir(dir);
 	}
-	free(foot);
 	return (NULL);
 }
 
-void	replace(t_rl *rl, char *replaced, char *replacement) {
-	size_t	len = strcspn(replaced, " <>&|");
-	size_t	offset = strlen(replacement) - len;
+void	init_comp(t_comp *comp, t_rl *rl) {
+	int	temp = rl->i + strcspn(rl->line + rl->i, DELIM);
 
-	memmove(replaced + offset, replaced, strlen(replaced) + 1);
-	memcpy(replaced, replacement, strlen(replacement));
-	rl->len += offset;
-	rl->i += offset;
+	comp->word_end = rl->line + temp;
+	comp->last_slash = NULL;
+	while (temp > 0 && !is_delim(rl->line[temp])) {
+		if (!comp->last_slash && rl->line[temp] == '/')
+			comp->last_slash = rl->line + temp;
+		temp--;
+	}
+	comp->word_start = rl->line + temp;
+	comp->base = comp->last_slash ? comp->last_slash + 1 : comp->word_start;
+	comp->base_len = comp->word_end - comp->base;
+	comp->filler = find_match(comp);
+}
+
+void	replace(t_rl *rl, t_comp *comp) {
+	size_t	filler_len = strlen(comp->filler);
+
+	memmove(rl->line + rl->i + filler_len, rl->line + rl->i, strlen(rl->line + rl->i) + 1);
+	memcpy(rl->line + rl->i, comp->filler, filler_len);
+	rl->i += filler_len;
+	rl->len += filler_len;
+	write(2, comp->filler, filler_len);
 }
 
 void	ts_completion(t_rl *rl) {
-	char	*to_complete = find_current_word(rl->line, rl->i);
-	char	*last_slash = find_last_slash(to_complete);
-	char	*replaced = last_slash ? last_slash + 1 : to_complete;
-	char	*replacement = find_match(to_complete, last_slash/*, replaced*/);
+	t_comp	comp;
 
-	if (!replacement)
-		return ;
-	replace(rl, replaced, replacement);
-	printf("%s\n", rl->line);
+	init_comp(&comp, rl);
+	replace(rl, &comp);
 }
